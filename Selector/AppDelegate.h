379c8c48d76923a5840c4a3ba45aa0b558924568
roman1pr <iroman.pri@gmail.com>
2015-03-29 17:43:32 +0300

    Initial Commit

---
+++ Selector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Selector
+//
+//  Created by Роман Приискалов on 29/03/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
